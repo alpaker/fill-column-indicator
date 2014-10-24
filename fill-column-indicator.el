e724cb5b50649ef66fbe3d056ceeb6cb95d93c74
manu <manuel.lopez-ibanez@ulb.ac.be>
2014-10-24 17:00:26 +0200

    Issues with 'truncate-lines nil (Wrap at window edge) #26
    (setq-default fci-rule-column 80)
    (define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
    (global-fci-mode 1)

--- fill-column-indicator.el
+++ fill-column-indicator.el
@@ -89,15 +89,6 @@
 ;; Other Options
 ;; =============
 
-;; When `truncate-lines' is nil, the effect of drawing a fill-column rule is
-;; very odd looking. Indeed, it makes little sense to use a rule to indicate
-;; the position of the fill column in that case (the positions at which the
-;; fill column falls in the visual display space won't, in general, be
-;; collinear).  For this reason, fci-mode sets truncate-lines to t in buffers
-;; in which it is enabled and restores it to its previous value when
-;; disabled.  You can turn this feature off by setting
-;; `fci-handle-truncate-lines' to nil.
-
 ;; If `line-move-visual' is t, then vertical navigation can behave oddly in
 ;; several edge cases while fci-mode is enabled (this is due to a bug in
 ;; Emacs's C code).  Accordingly, fci-mode sets line-move-visual to nil in
@@ -109,7 +100,7 @@
 
 ;; Fci-mode needs free use of two characters (specifically, it needs the use
 ;; of two characters whose display table entries it can change
-;; arbitrarily).  Its defualt is to use the first two characters of the
+;; arbitrarily).  Its default is to use the first two characters of the
 ;; Private Use Area of the Unicode BMP, viz. U+E000 and U+E001.  If you need
 ;; to use those characters for some other purpose, set `fci-eol-char' and
 ;; `fci-blank-char' to different values.
@@ -297,17 +288,6 @@ function `fci-mode' is run."
   :group 'fill-column-indicator
   :type 'boolean)
 
-(defcustom fci-handle-truncate-lines t
-  "Whether fci-mode should set truncate-lines to t while enabled.
-If non-nil, fci-mode will set truncate-lines to t in buffers in
-which it is enabled, and restore it to its previous value when
-disabled.
-
-Leaving this option set to the default value is recommended."
-  :group 'fill-column-indicator
-  :tag "Locally set truncate-lines to t during fci-mode"
-  :type 'boolean)
-
 (defcustom fci-handle-line-move-visual (version<= "23" emacs-version)
   "Whether fci-mode should set line-move-visual to nil while enabled.
 If non-nil, fci-mode will set line-move-visual to nil in buffers
@@ -344,7 +324,6 @@ U+E000-U+F8FF, inclusive)."
 ;; Record prior state of buffer.
 (defvar fci-saved-line-move-visual)
 (defvar fci-line-move-visual-was-buffer-local)
-(defvar fci-saved-truncate-lines)
 (defvar fci-saved-eol)
 (defvar fci-made-display-table)
 
@@ -370,7 +349,6 @@ U+E000-U+F8FF, inclusive)."
 ;; the mode is disabled.
 (defconst fci-internal-vars '(fci-saved-line-move-visual
                               fci-line-move-visual-was-buffer-local
-                              fci-saved-truncate-lines
                               fci-saved-eol
                               fci-made-display-table
                               fci-display-table-processed
@@ -591,9 +569,6 @@ on troubleshooting.)"
                 fci-saved-line-move-visual line-move-visual
                 line-move-visual nil)
         (set (make-local-variable 'line-move-visual) nil)))
-    (when fci-handle-truncate-lines
-      (setq fci-saved-truncate-lines truncate-lines
-            truncate-lines t))
     (setq fci-local-vars-set t)))
 
 (defun fci-make-rule-string ()
@@ -726,8 +701,7 @@ rough heuristic.)"
       (if fci-line-move-visual-was-buffer-local
           (setq line-move-visual fci-saved-line-move-visual)
         (kill-local-variable 'line-move-visual)))
-    (when fci-handle-truncate-lines
-      (setq truncate-lines fci-saved-truncate-lines))))
+    ))
 
 (defun fci-restore-display-table ()
   "Restore the buffer display table when fci-mode is disabled."
@@ -752,6 +726,12 @@ rough heuristic.)"
 (defun fci-delete-overlays-region (start end)
   "Delete overlays displaying the fill-column rule between START and END."
   (mapc #'(lambda (o) (if (overlay-get o 'fci) (delete-overlay o)))
+        (overlays-in start end)))
+
+(defun fci-delete-overlays-region-win (start end win)
+  "Delete overlays displaying the fill-column rule between START and END."
+  (mapc #'(lambda (o) (if (and (overlay-get o 'fci) (equal win (overlay-get o 'fci-win)))
+                          (delete-overlay o)))
         (overlays-in start end)))
 
 (defun fci-delete-overlays-buffer ()
@@ -783,7 +763,7 @@ rough heuristic.)"
 ;; only if we maintained the overlay center at an early position in the
 ;; buffer.  Since other packages that use overlays typically place them while
 ;; traversing the buffer in a forward direction, that would be a bad idea.
-(defun fci-put-overlays-region (start end)
+(defun fci-put-overlays-region (start end win)
   "Place overlays displaying the fill-column rule between START and END."
   (goto-char start)
   (let (o cc)
@@ -792,6 +772,8 @@ rough heuristic.)"
       (setq cc (current-column)
             o (make-overlay (match-beginning 0) (match-beginning 0)))
       (overlay-put o 'fci t)
+      (overlay-put o 'fci-win win)
+      (overlay-put o 'window win)
       (cond
        ((< cc fci-limit)
         (overlay-put o 'after-string fci-pre-limit-string))
@@ -801,6 +783,17 @@ rough heuristic.)"
         (overlay-put o 'after-string fci-at-limit-string)))
       (goto-char (match-end 0)))))
 
+(defun fci-redraw-region-win (start end _ignored win)
+  "Erase and redraw the fill-column rule between START and END."
+  (save-match-data
+    (save-excursion
+      (let ((inhibit-point-motion-hooks t))
+        (goto-char end)
+        (setq end (line-beginning-position 2))
+        (fci-delete-overlays-region-win start end win)
+        (if (> (window-width win) fci-rule-column)
+            (fci-put-overlays-region start end win))))))
+
 (defun fci-redraw-region (start end _ignored)
   "Erase and redraw the fill-column rule between START and END."
   (save-match-data
@@ -808,12 +801,13 @@ rough heuristic.)"
       (let ((inhibit-point-motion-hooks t))
         (goto-char end)
         (setq end (line-beginning-position 2))
-        (fci-delete-overlays-region start end)
-        (fci-put-overlays-region start end))))) 
+        (fci-delete-overlays-region-win start end (selected-window))
+        (if (> (window-width (selected-window)) fci-rule-column)
+            (fci-put-overlays-region start end (selected-window)))))))
 
 (defun fci-redraw-window (win &optional start)
   "Redraw the fill-column rule in WIN starting from START."
-  (fci-redraw-region (or start (window-start win)) (window-end win t) 'ignored))
+  (fci-redraw-region-win (or start (window-start win)) (window-end win t) 'ignored win))
 
 ;; This doesn't determine the strictly minimum amount by which the rule needs
 ;; to be extended, but the amount used is always sufficient, and determining

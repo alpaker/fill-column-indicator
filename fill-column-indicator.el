5afad73d02912e8349d7aca1d6eaf7942cd6872c
Jonas Bernoulli <jonas@bernoul.li>
2017-09-04 20:56:25 +0200

    Silence byte-compiler
    The function `line-number-display-width' also isn't available
    in Emacsen before v26, so the byte-compiler complains about it
    unless told otherwise.

--- fill-column-indicator.el
+++ fill-column-indicator.el
@@ -413,7 +413,8 @@ U+E000-U+F8FF, inclusive)."
 (defun fci-determine-padding ()
   "Decide how much padding the overlay needs.
 When `display-line-numbers` is true, pad by the size of the line number display."
-  (if (and (boundp 'display-line-numbers) display-line-numbers)
+  (if (and (bound-and-true-p display-line-numbers)
+           (fboundp 'line-number-display-width))
       (+ (line-number-display-width) 2)
     0))
 
